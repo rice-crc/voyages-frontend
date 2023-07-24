ed308b504e7f6bbc59a7f6734115670451560489
John Connor <jcm10@rice.edu>
2023-07-24 18:13:40 -0500

    provisional fix to nested data problem

--- src/utils/functions/traverseData.ts
+++ src/utils/functions/traverseData.ts
@@ -32,3 +32,41 @@ export const traverseData = (data: any, varArray: string[]): any => {
     }
     return data ?? null
 };
+
+export const traverseData2 = (data: any, varArray: string[]): any=> {
+	
+	var output = traverseData(data, varArray);
+	
+// 	console.log("raw output", output,! output===null);
+	if (output!=null){
+		if (output.length > 0 && Array.isArray(output[0])){
+			console.log("checking-->",output[0],Array.isArray(output[0]))
+			if (Array.isArray(output[0])) {
+				console.log("flattening",output)
+				output=output.flat(3)
+			
+			}
+		
+	// 		var new_output = [];
+	// 		
+	// 		if (Array.isArray(output[0])) {
+	// 			console.log(output[0])
+		
+	// 		  output[0].forEach((a) => {
+	// 		  	if (Array.isArray(a)){
+	// 				a.forEach((i) => {
+	// 				  new_output.push(i);
+	// 				});
+	// 			} else {
+	// 				new_output.push(a)
+	// 			}
+	// 		  });
+	// 		  output = new_output;
+	// 		}
+		
+		}
+	}
+	return output ?? null
+	
+	
+}
