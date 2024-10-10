c927188ea9461b8525ec03c52f1b2fcc10632c7f
MeowMerry <meow3525@gmail.com>
2024-10-10 14:06:19 -0500

    DD-0251 : "Linked Voyages" under "Itinerary" of Production is missing in its corresponding variable group in the Redesign

--- src/components/SelectorComponents/ButtonComponents/ButtonDropdownColumnSelector.tsx
+++ src/components/SelectorComponents/ButtonComponents/ButtonDropdownColumnSelector.tsx
@@ -1,13 +1,13 @@
-import { ArrowDropDown, ArrowRight } from '@mui/icons-material';
-import { Button } from '@mui/material';
+import {ArrowDropDown, ArrowRight} from '@mui/icons-material';
+import {Button} from '@mui/material';
 import {
   DropdownMenuItem,
   DropdownNestedMenuItemChildren,
 } from '@/styleMUI';
-import { MouseEvent, useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '@/redux/store';
-import { setVisibleColumn } from '@/redux/getColumnSlice';
+import {MouseEvent, useEffect, useState} from 'react';
+import {useDispatch, useSelector} from 'react-redux';
+import {AppDispatch, RootState} from '@/redux/store';
+import {setVisibleColumn} from '@/redux/getColumnSlice';
 import {
   ColumnSelectorTree,
   TableCellStructureInitialStateProp,
@@ -19,23 +19,23 @@ import Transatlantic_TABLE_FLAT from '@/utils/flatfiles/voyages/voyages_transatl
 import Intraamerican_TABLE_FLAT from '@/utils/flatfiles/voyages/voyages_intraamerican_table.json';
 import AllVoyages_TABLE_FLAT from '@/utils/flatfiles/voyages/voyages_all_table.json';
 import ENSLAVERS_TABLE from '@/utils/flatfiles/enslavers/enslavers_table.json';
-import { LabelFilterMeneList, TYPESOFDATASET, TYPESOFDATASETPEOPLE } from '@/share/InterfaceTypes';
-import { usePageRouter } from '@/hooks/usePageRouter';
-import { checkRouteForVoyages } from '@/utils/functions/checkPagesRoute';
-import { ENSALVERSTYLE, INTRAAMERICANTRADS, TRANSATLANTICTRADS } from '@/share/CONST_DATA';
-import { DropdownCascading } from '../Cascading/DropdownCascading';
-import { getColorBTNVoyageDatasetBackground, getColorBoxShadow, getColorHoverBackground } from '@/utils/functions/getColorStyle';
-import { translationHomepage } from '@/utils/functions/translationLanguages';
+import {LabelFilterMeneList, TYPESOFDATASET, TYPESOFDATASETPEOPLE} from '@/share/InterfaceTypes';
+import {usePageRouter} from '@/hooks/usePageRouter';
+import {checkRouteForVoyages} from '@/utils/functions/checkPagesRoute';
+import {ENSALVERSTYLE, INTRAAMERICANTRADS, TRANSATLANTICTRADS} from '@/share/CONST_DATA';
+import {DropdownCascading} from '../Cascading/DropdownCascading';
+import {getColorBTNVoyageDatasetBackground, getColorBoxShadow, getColorHoverBackground} from '@/utils/functions/getColorStyle';
+import {translationHomepage} from '@/utils/functions/translationLanguages';
 
 const ButtonDropdownColumnSelector = () => {
   const dispatch: AppDispatch = useDispatch();
-  const { styleName: styleNameRoute } = usePageRouter()
-  const { visibleColumnCells } = useSelector(
+  const {styleName: styleNameRoute} = usePageRouter();
+  const {visibleColumnCells} = useSelector(
     (state: RootState) => state.getColumns as TableCellStructureInitialStateProp
   );
-  const { languageValue } = useSelector((state: RootState) => state.getLanguages);
+  const {languageValue} = useSelector((state: RootState) => state.getLanguages);
   const [menuValueCells, setMenuValueCells] = useState<ColumnSelectorTree[]>([]);
-  const translatedHomepage = translationHomepage(languageValue)
+  const translatedHomepage = translationHomepage(languageValue);
 
 
 
@@ -52,23 +52,23 @@ const ButtonDropdownColumnSelector = () => {
     }
   };
 
-  const transatlanticColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(Transatlantic_TABLE_FLAT.column_selector_tree))
-  const intraamericanColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(Intraamerican_TABLE_FLAT.column_selector_tree))
-  const allVoyageColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(AllVoyages_TABLE_FLAT.column_selector_tree))
-  const enslavedColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(ENSLAVED_TABLE.column_selector_tree))
-  const africanOriginsColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(AFRICANORIGINS_TABLE.column_selector_tree))
-  const texasColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(TEXAS_TABLE.column_selector_tree))
-  const enslaversColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(ENSLAVERS_TABLE.column_selector_tree))
+  const transatlanticColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(Transatlantic_TABLE_FLAT.column_selector_tree));
+  const intraamericanColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(Intraamerican_TABLE_FLAT.column_selector_tree));
+  const allVoyageColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(AllVoyages_TABLE_FLAT.column_selector_tree));
+  const enslavedColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(ENSLAVED_TABLE.column_selector_tree));
+  const africanOriginsColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(AFRICANORIGINS_TABLE.column_selector_tree));
+  const texasColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(TEXAS_TABLE.column_selector_tree));
+  const enslaversColumnSelector: ColumnSelectorTree[] = JSON.parse(JSON.stringify(ENSLAVERS_TABLE.column_selector_tree));
 
   useEffect(() => {
     const loadMenuValueCellStructure = async () => {
       try {
         if (styleNameRoute === TYPESOFDATASET.transatlantic) {
-          setMenuValueCells(transatlanticColumnSelector)
+          setMenuValueCells(transatlanticColumnSelector);
         } else if (styleNameRoute === TYPESOFDATASET.intraAmerican) {
-          setMenuValueCells(intraamericanColumnSelector)
+          setMenuValueCells(intraamericanColumnSelector);
         } else if (checkRouteForVoyages(styleNameRoute!)) {
-          setMenuValueCells(allVoyageColumnSelector)
+          setMenuValueCells(allVoyageColumnSelector);
         } else if (styleNameRoute === TYPESOFDATASETPEOPLE.allEnslaved) {
           setMenuValueCells(enslavedColumnSelector);
         } else if (styleNameRoute === TYPESOFDATASETPEOPLE.africanOrigins) {
@@ -88,7 +88,7 @@ const ButtonDropdownColumnSelector = () => {
 
   function renderMenuItems(nodes: any[]) {
     return nodes.map((node) => {
-      const { label: nodeLabel, children, var_name, colID } = node;
+      const {label: nodeLabel, children, var_name, colID} = node;
       const hasChildren = children && children.length > 0;
       const menuLabel = (nodeLabel as LabelFilterMeneList)[languageValue];
 
@@ -100,7 +100,7 @@ const ButtonDropdownColumnSelector = () => {
             data-colid={colID}
             data-value={var_name}
             data-label={menuLabel}
-            rightIcon={<ArrowRight style={{ fontSize: 15 }} />}
+            rightIcon={<ArrowRight style={{fontSize: 15}} />}
             onClickMenu={handleColumnVisibilityChange}
             menu={renderMenuItems(children)}
             disabled={visibleColumnCells.includes(colID)}
@@ -125,7 +125,7 @@ const ButtonDropdownColumnSelector = () => {
   return (
     <DropdownCascading
       trigger={
-        <span style={{ display: 'flex', alignItems: 'center' }}>
+        <span style={{display: 'flex', alignItems: 'center'}}>
           <Button
             sx={{
               fontSize: '0.80rem',
@@ -134,7 +134,7 @@ const ButtonDropdownColumnSelector = () => {
               boxShadow: getColorBoxShadow(styleNameRoute!),
               fontWeight: 600,
               color: '#ffffff',
-              width: { xs: 180, sm: 180 },
+              width: {xs: 180, sm: 180},
               '&:hover': {
                 backgroundColor: getColorHoverBackground(styleNameRoute!),
               },
