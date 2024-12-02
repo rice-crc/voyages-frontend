83babbff10cd4dd23d0ae273925f9ebd27358075
Meowmerry <meow3525@gmail.com>
2024-12-01 18:23:34 -0600

    pull

--- src/components/PresentationComponents/Contribute/Guidelines.tsx
+++ src/components/PresentationComponents/Contribute/Guidelines.tsx
@@ -1,17 +1,13 @@
 import '@/style/contributeContent.scss';
 
 const Guidelines: React.FC = () => {
-    return (
-        <div className="contribute-content">
-        <h1 className="page-title-1">Guidelines for Contributors</h1>
-        <p>
-        The guide is provided as a PDF file to allow users to reference it in a separate window or to download and print it.
-        </p>
-        <p>
-        Guidelines
-        </p>
-      </div>
-   )
+  return (
+    <div className="contribute-content">
+      <h1 className="page-title-1">Guidelines for Contributors</h1>
+      <p> The guide is provided as a PDF file to allow users to reference it in a separate window or to download and print it. </p>
+      <p> Guidelines </p>
+    </div>
+  )
 
 };
 export default Guidelines;
