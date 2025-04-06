61150e1a8b5d1341e2a22cc92320d144bd4e7b08
Meowmerry <meow3525@gmail.com>
2025-04-06 18:16:50 -0500

    cleaned up file

--- src/pages/Contribute 2.tsx
+++
@@ -1,6 +0,0 @@
-import ContributeComponent from '@/components/PresentationComponents/Contribute/Contribute';
-
-const ContributePage: React.FC = () => {
-  return <ContributeComponent />;
-};
-export default ContributePage;
