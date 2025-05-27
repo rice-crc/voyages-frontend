07a891b991f2c7f4973a5263690b194a9e670169
Meowmerry <meow3525@gmail.com>
2025-05-27 16:12:22 -0500

    Fixed MUI lose stlying after view document and close the view

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,37 @@
 import React, { useEffect, useState } from 'react';
+
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { StyledEngineProvider } from '@mui/material/styles';
+import { StylesProvider } from '@mui/styles';
 import { QueryClient, QueryClientProvider } from 'react-query';
-import { BrowserRouter, Route, Routes,useLocation } from 'react-router-dom';
-import { ThemeProvider } from '@mui/material';
-import VoyagesPage from '@/pages/VoyagesPage';
-import HomePage from '@/pages/HomePage';
-import PastHomePage from '@/pages/PastHomePage';
+import { useDispatch, useSelector } from 'react-redux';
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
+
+import BlogDetailsPost from '@/components/BlogPageComponents/Blogcomponents/BlogDetailsPost';
+import Estimates from '@/components/PresentationComponents/Assessment/Estimates/Estimates';
+import IntroductoryMaps from '@/components/PresentationComponents/Assessment/IntroductoryMaps/IntroductoryMaps';
+import LessonPlans from '@/components/PresentationComponents/Assessment/LessonPlans/LessonPlans';
+import TabsSelect from '@/components/SelectorComponents/Tabs/TabsSelect';
+import { usePageRouter } from '@/hooks/usePageRouter';
+import AboutPage from '@/pages/AboutPage';
+import AuthorPage from '@/pages/AuthorPage';
+import BlogPage from '@/pages/BlogPage';
+import ContributePage from '@/pages/Contribute';
+import DocumentPage from '@/pages/DocumentPage';
+import DownloadPage from '@/pages/DownloadPage';
 import EnslavedHomePage from '@/pages/EnslavedPage';
 import EnslaversHomePage from '@/pages/EnslaversPage';
-import { theme } from '@/styleMUI/theme';
-import { useDispatch, useSelector } from 'react-redux';
+import HomePage from '@/pages/HomePage';
+import InstitutionAuthorsPage from '@/pages/InstitutionAuthorsPage';
+import PageNotFound404 from '@/pages/PageNotFound404';
+import PastHomePage from '@/pages/PastHomePage';
+import VoyagesPage from '@/pages/VoyagesPage';
+import {
+  setCardRowID,
+  setNodeClass,
+  setValueVariable,
+} from '@/redux/getCardFlatObjectSlice';
+import { RootState } from '@/redux/store';
 import {
   ABOUTPAGE,
   ACCOUNTS,
@@ -33,30 +56,11 @@ import {
   TRANSATLANTICPAGE,
   allEnslavers,
 } from '@/share/CONST_DATA';
-import PageNotFound404 from '@/pages/PageNotFound404';
-import BlogPage from '@/pages/BlogPage';
-import AuthorPage from '@/pages/AuthorPage';
-import InstitutionAuthorsPage from '@/pages/InstitutionAuthorsPage';
-import BlogDetailsPost from '@/components/BlogPageComponents/Blogcomponents/BlogDetailsPost';
-import Estimates from '@/components/PresentationComponents/Assessment/Estimates/Estimates';
+import { theme } from '@/styleMUI/theme';
 
-import LessonPlans from '@/components/PresentationComponents/Assessment/LessonPlans/LessonPlans';
-import IntroductoryMaps from '@/components/PresentationComponents/Assessment/IntroductoryMaps/IntroductoryMaps';
-import {
-  setCardRowID,
-  setNodeClass,
-  setValueVariable,
-} from '@/redux/getCardFlatObjectSlice';
-import { RootState } from '@/redux/store';
-import TabsSelect from '@/components/SelectorComponents/Tabs/TabsSelect';
-import { usePageRouter } from '@/hooks/usePageRouter';
-import DocumentPage from '@/pages/DocumentPage';
-import AboutPage from '@/pages/AboutPage';
-import DownloadPage from '@/pages/DownloadPage';
 import UseSaveSearchURL from './components/FilterComponents/SaveSearchComponent/SaveSearchURLReturn';
-import { checkEntityType } from './utils/functions/checkEntityType';
 import { DocumentViewerProvider } from './pages/DocumentViewerContext';
-import ContributePage from './pages/Contribute';
+import { checkEntityType } from './utils/functions/checkEntityType';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -71,7 +75,7 @@ const App: React.FC = () => {
   const dispatch = useDispatch();
   const location = useLocation();
   const { cardRowID, nodeTypeClass, variable } = useSelector(
-    (state: RootState) => state.getCardFlatObjectData
+    (state: RootState) => state.getCardFlatObjectData,
   );
   const { styleName, voyageURLID, blogURL } = usePageRouter();
 
@@ -115,122 +119,100 @@ const App: React.FC = () => {
   ]);
 
   return (
-    <ThemeProvider theme={theme}>
-      <QueryClientProvider client={queryClient}>
-        <DocumentViewerProvider>
-          <Routes>
-            {nodeClass && ID && (
-              <Route path={`${nodeClass}/${ID}`} element={<TabsSelect />} />
-            )}
-            <Route path="/" element={<HomePage />} />
-            {saveSearchURL && nodeClass && (
-              <Route path={`${saveSearchURL}`} element={<UseSaveSearchURL />} />
-            )}
-            <Route path={`${TRANSATLANTICPAGE}`} element={<VoyagesPage />} />
-            <Route path={`${INTRAAMERICANPAGE}`} element={<VoyagesPage />} />
-            <Route path={`${ALLVOYAGESPAGE}`} element={<VoyagesPage />} />
-            <Route path={`${PASTHOMEPAGE}`} element={<PastHomePage />} />
-            <Route
-              path={`${ENSALVEDPAGE}${ALLENSLAVEDPAGE}`}
-              element={<EnslavedHomePage />}
-            />
-            <Route
-              path={`${ENSALVEDPAGE}${AFRICANORIGINSPAGE}`}
-              element={<EnslavedHomePage />}
-            />
-            <Route
-              path={`${ENSALVEDPAGE}${ENSLAVEDTEXASPAGE}`}
-              element={<EnslavedHomePage />}
-            />
-            <Route
-              path={`${ENSALVERSPAGE}${INTRAAMERICANENSLAVERS}`}
-              element={<EnslaversHomePage />}
-            />
-            <Route
-              path={`${ENSALVERSPAGE}${TRANSATLANTICENSLAVERS}`}
-              element={<EnslaversHomePage />}
-            />
-            <Route
-              path={`${ENSALVERSPAGE}/${allEnslavers}`}
-              element={<EnslaversHomePage />}
-            />
-            <Route
-              path={`${ASSESSMENT}/${ESTIMATES}/`}
-              element={<Estimates />}
-            />
-            <Route path={`${DOCUMENTPAGE}`} element={<DocumentPage />} />
-            <Route path={`${BLOGPAGE}/`} element={<BlogPage />} />
-            <Route path={`${BLOGPAGE}/tag/${blogURL}`} element={<BlogPage />} />
-
-            <Route
-              path={`${BLOGPAGE}/:blogTitle/:ID`}
-              element={<BlogDetailsPost />}
-            />
-            <Route
-              path={`${BLOGPAGE}/author/:authorName/:ID/`}
-              element={<AuthorPage />}
-            />
-            <Route
-              path={`${BLOGPAGE}/institution/:institutionName/:ID/`}
-              element={<InstitutionAuthorsPage />}
-            />
-            <Route path={`${CONTRIBUTE}`} element={<ContributePage />} />
-            <Route
-              path={`${CONTRIBUTE}guidelines`}
-              element={<ContributePage />}
-            />
-            <Route path={`${ACCOUNTS}signin`} element={<ContributePage />} />
-            <Route path={`${ACCOUNTS}signup`} element={<ContributePage />} />
-            <Route
-              path={`${ACCOUNTS}password/reset`}
-              element={<ContributePage />}
-            />
-            <Route path={`${ACCOUNTS}logout`} element={<ContributePage />} />
-            <Route
-              path={`${ACCOUNTS}password_change`}
-              element={<ContributePage />}
-            />
-            <Route path={`${CONTRIBUTE}legal`} element={<ContributePage />} />
-            <Route
-              path={`${CONTRIBUTE}interim/new/`}
-              element={<ContributePage />}
-            />
-            <Route
-              path={`${CONTRIBUTE}edit_voyage`}
-              element={<ContributePage />}
-            />
-            <Route
-              path={`${CONTRIBUTE}merge_voyages`}
-              element={<ContributePage />}
-            />
-            <Route
-              path={`${CONTRIBUTE}delete_voyage`}
-              element={<ContributePage />}
-            />
-            <Route path={`${LESSONPLANS}/`} element={<LessonPlans />} />
-            <Route
-              path={`${INTRODUCTORYMAPS}/`}
-              element={<IntroductoryMaps />}
-            />
-            <Route path={`${ABOUTPAGE}`} element={<AboutPage />} />
-            <Route path={`${DOWNLOADS}`} element={<DownloadPage />} />
-            <Route path="/404" element={<PageNotFound404 />} />
-            <Route path="*" element={<PageNotFound404 />} />
-          </Routes>
-        </DocumentViewerProvider>
-      </QueryClientProvider>
-    </ThemeProvider>
+    <Routes>
+      {nodeClass && ID && (
+        <Route path={`${nodeClass}/${ID}`} element={<TabsSelect />} />
+      )}
+      <Route path="/" element={<HomePage />} />
+      {saveSearchURL && nodeClass && (
+        <Route path={`${saveSearchURL}`} element={<UseSaveSearchURL />} />
+      )}
+      <Route path={`${TRANSATLANTICPAGE}`} element={<VoyagesPage />} />
+      <Route path={`${INTRAAMERICANPAGE}`} element={<VoyagesPage />} />
+      <Route path={`${ALLVOYAGESPAGE}`} element={<VoyagesPage />} />
+      <Route path={`${PASTHOMEPAGE}`} element={<PastHomePage />} />
+      <Route
+        path={`${ENSALVEDPAGE}${ALLENSLAVEDPAGE}`}
+        element={<EnslavedHomePage />}
+      />
+      <Route
+        path={`${ENSALVEDPAGE}${AFRICANORIGINSPAGE}`}
+        element={<EnslavedHomePage />}
+      />
+      <Route
+        path={`${ENSALVEDPAGE}${ENSLAVEDTEXASPAGE}`}
+        element={<EnslavedHomePage />}
+      />
+      <Route
+        path={`${ENSALVERSPAGE}${INTRAAMERICANENSLAVERS}`}
+        element={<EnslaversHomePage />}
+      />
+      <Route
+        path={`${ENSALVERSPAGE}${TRANSATLANTICENSLAVERS}`}
+        element={<EnslaversHomePage />}
+      />
+      <Route
+        path={`${ENSALVERSPAGE}/${allEnslavers}`}
+        element={<EnslaversHomePage />}
+      />
+      <Route path={`${ASSESSMENT}/${ESTIMATES}/`} element={<Estimates />} />
+      <Route path={`${DOCUMENTPAGE}`} element={<DocumentPage />} />
+      <Route path={`${BLOGPAGE}/`} element={<BlogPage />} />
+      <Route path={`${BLOGPAGE}/tag/${blogURL}`} element={<BlogPage />} />
+      <Route
+        path={`${BLOGPAGE}/:blogTitle/:ID`}
+        element={<BlogDetailsPost />}
+      />
+      <Route
+        path={`${BLOGPAGE}/author/:authorName/:ID/`}
+        element={<AuthorPage />}
+      />
+      <Route
+        path={`${BLOGPAGE}/institution/:institutionName/:ID/`}
+        element={<InstitutionAuthorsPage />}
+      />
+      <Route path={`${CONTRIBUTE}`} element={<ContributePage />} />
+      <Route path={`${CONTRIBUTE}guidelines`} element={<ContributePage />} />
+      <Route path={`${ACCOUNTS}signin`} element={<ContributePage />} />
+      <Route path={`${ACCOUNTS}signup`} element={<ContributePage />} />
+      <Route path={`${ACCOUNTS}password/reset`} element={<ContributePage />} />
+      <Route path={`${ACCOUNTS}logout`} element={<ContributePage />} />
+      <Route path={`${ACCOUNTS}password_change`} element={<ContributePage />} />
+      <Route path={`${CONTRIBUTE}legal`} element={<ContributePage />} />
+      <Route path={`${CONTRIBUTE}interim/new/`} element={<ContributePage />} />
+      <Route path={`${CONTRIBUTE}edit_voyage`} element={<ContributePage />} />
+      <Route path={`${CONTRIBUTE}merge_voyages`} element={<ContributePage />} />
+      <Route path={`${CONTRIBUTE}delete_voyage`} element={<ContributePage />} />
+      <Route path={`${LESSONPLANS}/`} element={<LessonPlans />} />
+      <Route path={`${INTRODUCTORYMAPS}/`} element={<IntroductoryMaps />} />
+      <Route path={`${ABOUTPAGE}`} element={<AboutPage />} />
+      <Route path={`${DOWNLOADS}`} element={<DownloadPage />} />
+      <Route path="/404" element={<PageNotFound404 />} />
+      <Route path="*" element={<PageNotFound404 />} />
+    </Routes>
   );
 };
 
 const AppWithRouter: React.FC = () => (
-  <BrowserRouter future={{ 
-    v7_startTransition: true,
-    v7_relativeSplatPath: true 
-  }}>
-    <App />
+  <BrowserRouter
+    future={{
+      v7_startTransition: true,
+      v7_relativeSplatPath: true,
+    }}
+  >
+    <StyledEngineProvider injectFirst>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <QueryClientProvider client={queryClient}>
+            <DocumentViewerProvider>
+              <App />
+            </DocumentViewerProvider>
+          </QueryClientProvider>
+        </ThemeProvider>
+      </StylesProvider>
+    </StyledEngineProvider>
   </BrowserRouter>
 );
 
 export default AppWithRouter;
-
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,27 @@
 import React from 'react';
+
+import { StyledEngineProvider } from '@mui/material/styles';
 import ReactDOM from 'react-dom/client';
-import store from './redux/store';
 import '@fortawesome/fontawesome-free/css/all.css';
 import 'leaflet/dist/leaflet.css';
 import './style/index.css';
 import { Provider } from 'react-redux';
+import { StyleSheetManager } from 'styled-components';
+
 import AppWithRouter from './App.js';
-ReactDOM.createRoot(
-  document.getElementById('root') as HTMLAnchorElement
-).render(
+import store from './redux/store';
+
+const insertionPoint =
+  document.getElementById('styled-components-insertion-point') ?? undefined;
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <AppWithRouter />
-    </Provider>
-  </React.StrictMode>
+    <StyledEngineProvider injectFirst>
+      <StyleSheetManager target={insertionPoint}>
+        <Provider store={store}>
+          <AppWithRouter />
+        </Provider>
+      </StyleSheetManager>
+    </StyledEngineProvider>
+  </React.StrictMode>,
 );
