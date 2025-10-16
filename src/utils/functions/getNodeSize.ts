b554c59ec6f0912084cb3a1357495d16d523d24d
Meowmerry <meow3525@gmail.com>
2025-10-15 19:44:12 -0500

    - Node at Network graph size - Tabs don't show up on voyages page around certain break points (see screenshot) - Add a network graph tab & hide the images tab - ADD SEO Meta: Voyage, Enslaver, and Enslaved individual views,

--- src/utils/functions/getNodeSize.ts
+++ src/utils/functions/getNodeSize.ts
@@ -1,13 +1,13 @@
 import { EdgesAggroutes, NodeAggroutes } from '@/share/InterfaceTypesMap';
 
-export function getNodeSize(node: NodeAggroutes): number | number {
+export function getNodeSize(node: NodeAggroutes): number {
   const weightsVal = Object.keys(node.weights).length !== 0;
-
   if (!weightsVal) return 0;
 
   const weights = node.weights;
-  const originValue = weights.origin !== undefined ? weights.origin : 0;
-  const postDisembarkationValue = weights?.disembarkation ?? 0;
+  const originValue = weights.origin ?? 0;
+  const postDisembarkationValue = weights.post_disembarkation ?? 0; 
+
   const size =
     (weights.disembarkation ?? 0) +
     (weights.embarkation ?? 0) +
